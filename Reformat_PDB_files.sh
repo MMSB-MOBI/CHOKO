be219c1a727caba2cac7af6d3fc8bcde1591142a
juliette martin <juliette.martin@ibcp.fr>
2020-03-10 15:47:01 +0100

    added Reformat shell script

---
+++ Reformat_PDB_files.sh
@@ -0,0 +1,5 @@
+tar xzvf decoys_bm4_zd3.0.2_6deg_fixed.tar.gz 
+cd decoys_bm4_zd3.0.2_6deg_fixed/input_pdbs/
+        # reformat to have a parsable PDB format
+        for file in `ls *.pdb.ms `; do cut -c1-55 $file > ${file%%.ms}; done
+
