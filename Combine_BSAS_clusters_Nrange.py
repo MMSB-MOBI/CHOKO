c09528586de4dc5a05003ad5b047375d0923cc40
juliette martin <juliette.martin@ibcp.fr>
2020-05-04 15:58:49 +0200

    update functions to the new version of DockingPP

--- Combine_BSAS_clusters_Nrange.py
+++ Combine_BSAS_clusters_Nrange.py
@@ -1,8 +1,5 @@
 import sys
-
-from DockingPP.core import zParse
-from DockingPP.core_clustering import BSAS
-from DockingPP.core_scores import countNative
+import DockingPP
 import argparse
 
 def args_gestion():
@@ -13,7 +10,7 @@ def args_gestion():
 	parser.add_argument("--zdock_results", metavar = "<dir>", help = "Directory with zdock results", required = True)
 	parser.add_argument("--maxD", metavar = "<int>", help = "cutoff for the clustering (default 8)", default=8, type=float)
 	parser.add_argument("--max_pose", metavar = "<int>", help = "Number of poses to keep (default: 2000)", default = 2000, type=int)
-	parser.add_argument("--all_scores", metavar = "<dir>", help = "Directory with all scores computed", required = True)
+	parser.add_argument("--score_dir", metavar = "<dir>", help = "Directory with all scores computed", required = True)
 	parser.add_argument("--top_N", metavar = "<int>", help = "topN to consider (default 10)", default = 10, type=int)
 	parser.add_argument("--rmsd", metavar = "<float>", help = "RMSD cutoff (default 2.5)", default = 2.5, type=float)
 	parser.add_argument("--verbose", metavar = "<str>", help = "verbose output (default False)", default="False",required = False)
@@ -40,37 +37,32 @@ if __name__ == "__main__":
 		with open(ARGS.list_complex) as f:
 			for prot in f: 
 				prot=prot.strip()
-				DD=zParse(ARGS.zdock_results + "/" + prot + ".zd3.0.2.fg.fixed.out", maxPose = ARGS.max_pose)
-				DD.setScores(filename = ARGS.all_scores + "/" + prot + ".tsv")
-				# read the RMSD file 
-				with open(ARGS.zdock_results + "/" + prot + ".zd3.0.2.fg.fixed.out.rmsds") as f:
+				DH=DockingPP.loadZdock(ARGS.zdock_results + "/" + prot+".zd3.0.2.fg.fixed.out",ARGS.max_pose)
+				DH.loadScores(ARGS.score_dir+"/"+prot+".tsv")
+
+            	# read the RMSD file 
+				with open(ARGS.zdock_results + "/" + prot +".zd3.0.2.fg.fixed.out.rmsds") as f:
 					lines=f.readlines()
-				data=[L.split()[1] for L in lines[0:ARGS.max_pose]]
-				# add it into the DD object, pose by pose 
-				for i in range(ARGS.max_pose):
-					DD.pList[i].set_RMSD(float(data[i]))	
-		
+            
+				rmsd={L.split()[0]:L.split()[1] for L in lines}
+
 				# cluster the poses using native ranks :
-				native_ranked_poses=[pose for pose in DD.rankedPoses()]
-				new_ranked_poses=DD.rankedPoses(element=ARGS.score)
-				c_clusters1=BSAS(native_ranked_poses, ARGS.maxD, out='dict', stop=ARGS.max_pose)
-				NbClus=len(c_clusters1)
-				# Get first pose (representative) for each cluster ? 
-				my_list1=[c_clusters1[i+1][0] for i in range(NbClus)]
+				DH.clusterPoses("original", ARGS.maxD, ARGS.max_pose)
+				representatives = DH.getRankedClusterRepresentatives("original")
+				my_list1=[clust[1].index for clust in representatives[:ARGS.top_N]]
 
 
-				c_clusters2=BSAS(new_ranked_poses, ARGS.maxD, out='dict', stop=ARGS.max_pose)
-				NbClus = len(c_clusters2)
-				my_list2=[c_clusters2[i+1][0] for  i in range(NbClus)]
-
-
+				DH.clusterPoses(ARGS.score, ARGS.maxD, ARGS.max_pose)
+				representatives = DH.getRankedClusterRepresentatives(ARGS.score)
+				my_list2=[clust[1].index for clust in representatives[:ARGS.top_N]]
 
 				if ARGS.N_native>-1:
 					n_native=ARGS.N_native
 					my_added_poses=[p for p in my_list2 if p not in my_list1[:n_native]][0:(ARGS.top_N-n_native)]
 					my_list_final=my_list1[:n_native]+my_added_poses
-					rmsds=DD.scores.rankedRmsds(my_list_final)
-					NB=countNative(rmsds, cutoff=ARGS.rmsd)[ARGS.top_N]
+
+					topN_rmsd=[rmsd[str(i)] for i in my_list_final]
+					NB=sum([1 for rmsd in topN_rmsd if float(rmsd)<ARGS.rmsd])
 					Results[n_native][prot]=NB
 					Poses[n_native][prot]=my_list_final
 
@@ -81,8 +73,10 @@ if __name__ == "__main__":
 						# First Zdock clusters, then other clusters 
 						my_added_poses=[p for p in my_list2 if p not in my_list1[:n_native]][0:(ARGS.top_N-n_native)]
 						my_list_final=my_list1[:n_native]+my_added_poses
-						rmsds=DD.scores.rankedRmsds(my_list_final)
-						NB=countNative(rmsds, cutoff=ARGS.rmsd)[ARGS.top_N]
+
+
+						topN_rmsd=[rmsd[str(i)] for i in my_list_final]
+						NB=sum([1 for rmsd in topN_rmsd if float(rmsd)<ARGS.rmsd])
 						Results[n_native][prot]=NB
 						Poses[n_native][prot]=my_list_final
 						if NB>0:
--- Evaluate_cluster_combination.sh
+++ Evaluate_cluster_combination.sh
@@ -3,10 +3,11 @@ echo "Compute Number of successes with each scoring function after BSAS clusteri
 echo "frequency_set Score N_ZDOCK NB_success" > NB_success_cluster_combination.txt
 for N in 50 100 1000 2000
 do
-    for SCORE in contact_average contact_sum residue_average residue_sum
+    for SCORE in contacts_average contacts_sum residues_average residues_sum
     do
-    python $SRC_DIR/Combine_BSAS_clusters_Nrange.py --score $SCORE --list $SRC_DIR/listBM.txt  --zdock_results $ZDOCK_DIR/results/ --max_pose 2000 --maxD 8 --all_scores Scores_Freq_top$N/  --rmsd 2.5 > result.temp
+    python $SRC_DIR/Combine_BSAS_clusters_Nrange.py --score $SCORE --list $SRC_DIR/listBM.txt  --zdock_results $ZDOCK_DIR/results/ --max_pose 2000 --maxD 8 --score_dir SCORES/Freq_top$N/  --rmsd 2.5 > result.temp
     awk -v header="$N $SCORE" '{print header" "$0}' result.temp 
+    rm -f result.temp
     done
 done >> NB_success_cluster_combination.txt
 
