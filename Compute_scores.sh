77699910da954b67c779123cf95629dd5ef0d4e7
juliette martin <juliette.martin@ibcp.fr>
2020-05-04 13:51:50 +0200

    Merge step 1 and 2

--- Compute_scores.sh
+++ Compute_scores.sh
@@ -1,20 +1,15 @@
-for N in 50 100 1000 2000 
+mkdir SCORES
+cd SCORES
+
+for N1 in 50 100 1000 2000 
 do
-
-mkdir Scores_Freq_top$N
-cd Scores_Freq_top$N
-
-    for protein in `cat $SRC_DIR/listBM.txt `
-    do
-    echo $protein > list.temp
-        if [ ! -e $protein.tsv ]
-        then
-        echo Scores_Freq_top$N $protein 
-        python $SRC_DIR/Compute_scores_from_pkl.py --list list.temp --zdock_results $ZDOCK_DIR/results/ --input_pdb $ZDOCK_DIR/input_pdbs/ --N 2000 --freq_dir ..//Freq_top$N
-        fi
-    done
-cd ..
+    mkdir Freq_top$N1
+    cd Freq_top$N1
+    echo Freq_top$N1
+    python $SRC_DIR/Compute_scores.py --list $SRC_DIR/listBM.txt --zdock_results $ZDOCK_DIR/results/ --input_pdb $ZDOCK_DIR/input_pdbs/  --N1 $N1 --N2 2000
+    cd ..
 done
+cd ..
 
 
 
