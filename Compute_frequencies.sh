8580ac845b441dd69380536a197d1496797f57df
juliette martin <juliette.martin@ibcp.fr>
2020-05-04 15:16:51 +0200

    deletion of Compute_frequencies.sh

--- Compute_frequencies.sh
+++
@@ -1,8 +0,0 @@
-for N in 50 100 1000 2000 
-do
-mkdir Freq_top$N
-cd Freq_top$N  
-python $SRC_DIR/Compute_frequencies.py --list $SRC_DIR/listBM.txt --zdock_results $ZDOCK_DIR/results/ --input_pdb $ZDOCK_DIR/input_pdbs/ --N $N
-cd ..
-done 
-
